97a31b308d56934ea093590478150c1c1bb4f050
799629 <799629@seq.org>
2019-09-06 08:50:29 -0700

    Lab 903 rotations preliminary

---
+++ Lab 906 rotations/ship.js
@@ -0,0 +1,63 @@
+class Ship{
+  constructor(x,y,dx,dy,id){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0);
+    this.clr = color(random(255),random(255),random(255));
+    this.angle = 0
+    this.id = id
+}
+
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
+}//run end
+render(){
+  fill(this.clr);
+  push();
+    translate(this.x,this.y);
+    rotate(this.angle);
+    triangle(-5,8,5,8,0,-8)
+  pop();
+}//render end
+
+checkEdges(){
+
+    if(this.loc.x < 0){
+      this.loc.x = width
+    }
+    if(this.loc.x > width){
+      this.loc.x = 0
+    }
+    if(this.loc.y < 0){
+      this.loc.y = height
+    }
+    if(this.loc.y > height){
+      this.loc.y = 0
+  }
+}//checkEdges end
+
+update(){
+  this.clr = color(random(255),random(255),random(255));
+  var distToMainball;
+  if(this.id >= 0){
+    distToMainball = this.loc.dist(mainBall.loc);
+    if(distToMainball < 100000){
+      //add attraction
+      this.acc = p5.Vector.sub(mainBall.loc, this.loc);
+      this.acc.normalize();
+      this.acc.mult(0.1);
+    }
+    if(distToMainball < 150){
+      //add repulsion
+      this.acc = p5.Vector.sub(this.loc, mainBall.loc);
+      this.acc.normalize();
+      this.acc.mult(0.5);
+    }
+    this.vel.add(this.acc);
+  }
+  this.loc.add(this.vel);
+  this.vel.limit(5);
+}//update end
+}//Class end
