0d603dbb81aac90cde67de7d2ffb26e2b353ffb2
799629 <799629@seq.org>
2019-09-12 20:24:17 -0700

    nothing

---
+++ Lab 911 Paddle Game/ship.js
@@ -0,0 +1,66 @@
+class Ship{
+  constructor(x,y,dx,dy,id){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0);
+    this.clr = color(random(255),random(255),random(255));
+    this.angle = 0
+    this.id = id
+}
+
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
+}//run end
+
+render(){
+  fill(this.clr);
+  push();
+    translate(5,5);
+    rotate(this.loc.heading);
+    triangle(-5,8,5,8,0,-8)
+  pop();
+}//render end
+
+checkEdges(){
+
+    if(this.loc.x < 0){
+      this.loc.x = width
+    }
+    if(this.loc.x > width){
+      this.loc.x = 0
+    }
+    if(this.loc.y < 0){
+      this.loc.y = height
+    }
+    if(this.loc.y > height){
+      this.loc.y = 0
+  }
+}//checkEdges end
+
+update(){
+  var distToattractor;
+  var distTorepeller;
+  distToattractor = this.loc.dist(attractor.loc);
+  distTorepeller = this.loc.dist(repeller.loc);
+  if(this.id >= 0){
+    if(distToattractor < 100000){
+      //add attraction
+      this.acc = p5.Vector.sub(attractor.loc, this.loc);
+      this.acc.normalize();
+      this.acc.mult(0.1);
+    }
+    if(distTorepeller < 150){
+      //add repulsion
+      this.acc = p5.Vector.sub(this.loc, repeller.loc);
+      this.acc.normalize();
+      this.acc.mult(0.5);
+    }
+    this.vel.add(this.acc);
+  }
+  this.vel.add(this.acc);
+  this.loc.add(this.vel);
+  this.vel.limit(5);
+}//update end
+}//Class end
