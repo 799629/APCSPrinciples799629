1ceda36378f9d603def76892f4c2075d15a9a0a8
799629 <799629@seq.org>
2019-10-17 13:40:20 -0700

    Working Insertion Sort

--- Lab 1017 insertionSort/sketch.js
+++ Lab 1017 insertionSort/sketch.js
@@ -1,26 +1,40 @@
 //  Kawika Tu
 // 	9/5/19
 //  The setup function function is called once when your program begins
-var oldlist = [10,9,5,3,2,1,7,6,8,4]
+var oldlist = [];
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
+  oldlist = createIntList(oldlist, 100)
+  console.log("This is the origional list:")
+  printList(oldlist)
   newlist = insertionSort(oldlist);
+  console.log("This is the new sorted list:")
   printList(newlist);
 }//end setup
 
 function insertionSort(list){
-
-return(list)
+  for(var i = 0; i < list.length; i++){
+    if(list[i] > list[i+1]){
+      swap(list,i,i+1)
+    }
+    for(var j = i; j > 0; j--){
+      if(list[j] < list[j-1]){
+        swap(list,j,j-1)
+      }// end if statement
+    }//end j for loop
+  }//end i for loop
+  return(list)
 }
 
-function createIntList(n){
-  var intlist = [];
+function createIntList(list, n){
+  list = [];
   for(var i = 0; i < n; i++){
-    intlist.push(int(random(1,99)))
+    list.push(int(random(1,100)))
   }
+  return(list)
 }
 
 function swap(list, a, b){
