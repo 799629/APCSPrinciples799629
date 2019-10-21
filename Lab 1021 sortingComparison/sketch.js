f3b39cd580b3abb1bbdfa817a879eff2b60f5819
799629 <799629@seq.org>
2019-10-21 13:36:17 -0700

    Insertion Sort still doesn't work

--- Lab 1021 sortingComparison/sketch.js
+++ Lab 1021 sortingComparison/sketch.js
@@ -2,25 +2,25 @@
 // 	9/5/19
 //  The setup function function is called once when your program begins
 var oldlist = [];
-var selectionSwaps = 0
-var bubbleSwaps = 0
-var insertionSwaps = 0
-function setup() {
+var selectionSwaps = 0;
+var bubbleSwaps = 0;
+var insertionSwaps = 0;
+function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
   oldlist = createIntList(oldlist, 100)
   console.log("This is the origional list [randomized numbers]:")
-  printList(oldlist)
-  newlist = BubbleSort(oldlist)
-  newlist = insertionSort(oldlist)
+  printList(oldlist);
+  newlist = bubbleSort(oldlist);
   newlist = selectionSort(oldlist);
-  console.log("This is the new sorted list:")
+  newlist = InsertionSort(oldlist);
+  console.log("This is the new sorted list:");
   printList(newlist);
-  console.log("Bubble sort made " + str(bubbleSwaps) + " swaps")
-  console.log("Insertion sort made " + str(insertionSwaps) + " swaps")
-  console.log("Selection sort made " + str(selectionSwaps) + " swaps")
+  console.log("Bubble sort made " + str(bubbleSwaps) + " swaps");
+  console.log("Insertion sort made " + str(insertionSwaps) + " swaps");
+  console.log("Selection sort made " + str(selectionSwaps) + " swaps");
 }//end setup
 
 function selectionSort(list){
@@ -36,53 +36,53 @@ function selectionSort(list){
     list[index] = temp;
     selectionSwaps = selectionSwaps + 1
   }//end i for loop
-  return(list)
+  return(list);
 }//end function
 
-function BubbleSort(list){
+function bubbleSort(list){
   for (i=0; i<list.length-1; i++){
     for (j=0; j<list.length-1-i; j++){
       if (list[j+1] < list[j]) {
-        swap(list, j+1, j)
+        swap(list, j+1, j);
         bubbleSwaps = bubbleSwaps + 1
       }//end if statement
     }//end j for loop
   }//end i for loop
-  return (list)
+  return (list);
 }//end function
 
 function createIntList(list, n){
   list = [];
   for(var i = 0; i < n; i++){
-    list.push(int(random(1,100)))
+    list.push(int(random(1,100)));
   }
-  return(list)
+  return(list);
 }
 
-function insertionSort(list){
+function InsertionSort(list){
   for(var i = 0; i < list.length; i++){
     if(list[i] > list[i+1]){
       swap(list,i,i+1)
-      insertionSwaps = insertionSwaps + 1
     }//end if statement
     for(var j = i; j > 0; j--){
       if(list[j] < list[j-1]){
-        swap(list,j,j-1)
+        insertionSwaps = insertionSwaps + 1
+        swap(list,j,j-1);
       }// end if statement
     }//end j for loop
   }//end i for loop
-  return(list)
+  return(list);
 }
 
 function swap(list, a, b){
-  var temp =  list[a]
-  list[a] = list[b]
-  list[b] = temp
+  var temp =  list[a];
+  list[a] = list[b];
+  list[b] = temp;
   return(list[b],list[a])
 }
 
 function printList(list){
-  console.log(list)
+  console.log(list);
 }
 
 //function createList(){
