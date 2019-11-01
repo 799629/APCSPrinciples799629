25d750486a9ac7f1681903c984a3005279d009f5
799629 <799629@seq.org>
2019-11-01 08:48:27 -0700

    SuPr Sxy Slky SNky SneK

--- Project 2 testing code/sketch.js
+++ Project 2 testing code/sketch.js
@@ -2,17 +2,36 @@
 // 	10/31/19
 //  The setup function function is called once when your program begins
 var snakeHead;
+var score;
+var direction;
+var gameState = 1
 var snakeArray = [];
-function setup() {
+function setup(){
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
-  loadObjects(6);
+  startGame()
   score = 0;
+  snakeHead = new Snake(400,400,color(250),1,"head")
 }
 
 //  The draw function is called @ 30 fps
 function draw() {
+  frameRate(10)
+  background(5, 5, 5)
+  runGame()
+}
+
+function startGame(){
 
 }
+
+function runGame(){
+  for(var i = 0; i < snakeArray.length; i++){
+    snakeArray[i].run()
+
+  }//end for loop
+}//end function runGame
+
+//function ()
--- Project 2 testing code/snake.js
+++ Project 2 testing code/snake.js
@@ -2,29 +2,83 @@
 //09/03/19
 //Project 2 - Snake Game
 class Snake{
-  constructor(x,y){
+  constructor(x,y,clr,dir,id){
     this.loc = createVector(x,y)
     this.clr = color(random(255),random(255),random(255));
+    this.direction = dir
+    this.id = id
   }//constructor end
+
   run(){
+    this.control();
     this.update();
     this.checkEdges();
     this.render();
   }//run end
+
   render(){
     rectMode(CORNER)
     fill(this.clr);
-    rect(this.loc.x,this.loc.y,this.w,this.h)
+    rect(this.loc.x,this.loc.y,4,4)
   }//render end
+
+  control(){
+    if(keyCode() === UP_ARROW && this.direction !== 3){
+      this.direction = 1
+    }else{
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
+    }
+    if(keyCode() === RIGHT_ARROW && this.direction !== 4){
+      this.direction = 2
+    }else{
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
+    }
+    if(keyCode() === DOWN_ARROW && this.direction !== 1){
+      this.direction = 3
+    }else{
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
+    }
+    if(keyCode() === LEFT_ARROW && this.direction !== 2){
+      this.direction = 4
+    }else{
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
+    }
+  }//end control
+
   checkEdges(){
     if (this.loc.x < -1){
-      //endGame code
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
     }
-    if(this.loc.x > 801 - this.w){
-      //endGame code
+    if(this.loc.x > 801 - 5){
+      gameState = 0 //this needs to be changed Later
+      console.log("you died")
+    }
+    if(this.loc.y < -1){
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
+    }
+    if(this.loc.y > 801){
+      gameState = 0 //this needs to be changed later
+      console.log("you died")
     }
   }//checkEdges end
+
   update(){
-    
-  }
-  }//Class end
+    if(this.id === "head"){
+      if(this.direction === 1){
+        this.loc.y = this.loc.y - 4
+      }else if (this.direction === 2){
+        this.loc.x = this.loc.x + 4
+      }else if (this.direction === 3){
+        this.loc.y = this.loc.y + 4
+      }else if (this.direction === 4){
+        this.loc.x = this.loc.x - 4
+      }
+    }//end if this.id = head
+  }//update end
+}//Class end
