8201d8debc1a3a9fba282473082749d633870d4a
799629 <799629@seq.org>
2019-12-12 13:23:04 -0800

    Lab 1212 - Sliders for attraction and velocity

---
+++ Lab 1212 sliders attraction velocity/ship.js
@@ -0,0 +1,62 @@
+// Kawika Tu
+// 12/03/19
+class Ship{
+  constructor(x,y,dx,dy,id){
+    this.loc = createVector(x, y);
+    this.vel = createVector(dx, dy);
+    this.acc = createVector(0);
+    this.clr = color(random(255),random(255),random(255));
+    this.angle = 0
+    this.id = id
+}
+
+run(){
+  this.checkEdges();
+  this.update();
+  this.render();
+}//run end
+
+render(){
+  fill(this.clr);
+  push();
+    translate(this.loc.x,this.loc.y);
+    rotate(this.angle+HALF_PI);
+    triangle(-5,8,5,8,0,-8)
+  pop();
+}//render end
+
+checkEdges(){
+
+    if(this.loc.x < 0){
+      this.loc.x = width
+    }
+    if(this.loc.x > width){
+      this.loc.x = 0
+    }
+    if(this.loc.y < 0){
+      this.loc.y = height
+    }
+    if(this.loc.y > height){
+      this.loc.y = 0
+  }
+}//checkEdges end
+
+update(){
+  var distToattractor;
+  distToattractor = this.loc.dist(attractor.loc);
+  if(this.id >= 0){
+    if(distToattractor < 100000){
+      //add attraction
+      this.acc = p5.Vector.sub(attractor.loc, this.loc);
+      this.acc.normalize();
+      this.acc.mult(sliderAttraction.value());
+    }
+    this.vel.add(this.acc);
+  }
+  this.vel.add(this.acc);
+  this.loc.add(this.vel);
+  this.acc.limit(6)
+  this.vel.limit(sliderSpeed.value());
+  this.angle = this.vel.heading()
+}//update end
+}//Class end
---
+++ Lab 1212 sliders attraction velocity/sketch.js
@@ -0,0 +1,40 @@
+//  Kawika Tu
+// 	12/03/19
+//  The setup function function is called once when your program begins
+var ship;
+var attractor;
+var repeller;
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
+  loadPlanet();
+  loadShip();
+  sliderSpeed = createSlider(0, 10, 1, .0001);
+  sliderSpeed.position(100,400);
+  sliderAttraction = createSlider(0.0001, 0.9, 0.001, .0001);
+  sliderAttraction.position(100, 450)
+}
+
+//  The draw function is called @ 30 fps
+function draw() {
+  background(5, 5, 5, 14);
+  runBalls();
+  runShips();
+}
+function loadPlanet(){
+  attractor = new Planet(width/2, height/2, random(-2,2), random(-2,2), -1)
+}//loads balls into array
+
+function runBalls(){
+  attractor.run();
+}//runs the balls
+
+function loadShip(){
+  ship = new Ship(random(width), random(height), random(-1, 1), random(-1, 1), 1);
+}
+
+function runShips(){
+  ship.run();
+}//runs the ships
